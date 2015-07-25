07f5aa0608f09b6876ed19fe6f20be86d5c3a575
yoichi kobayashi <info@tplh.net>
2015-07-25 16:14:53 +0900

    [add] サイドカラム部分をモジュール化。

--- dst/wp/wp-content/themes/jstarted.com/index.php
+++ dst/wp/wp-content/themes/jstarted.com/index.php
@@ -55,56 +55,9 @@
           </div>
         </div>
         
-        <div class="sub">
-          <div class="sub-section writer">
-            <h2 class="sub-header">
-              Writer
-            </h2>
-            <figure class="writer-icon">
-              <img src="/img/elephant.png" alt="">
-            </figure>
-            <h3 class="writer-name">
-              Yoichi Kobayashi
-            </h3>
-            <p class="writer-profile">
-              東京ではたらくフロントエンドエンジニア。今はcanvas/物理/数学あたりを勉強中です。
-            </p>
-            <ul class="writer-account-list">
-              <li><a href="https://www.facebook.com/ykob1984" target="_blank">Facebook</a></li>
-              <li><a href="https://twitter.com/ykob0123" target="_blank">Twitter</a></li>
-              <li><a href="http://www.flickr.com/photos/goodmorning_ek/" target="_blank">Flickr</a></li>
-              <li><a href="https://github.com/ykob" target="_blank">GitHub</a></li>
-              <li><a href="http://codepen.io/ykob/" target="_blank">CodePen</a></li>
-            </ul>
-          </div>
-          
-          <div class="sub-section recommend-book">
-            <h2 class="sub-header">
-              Recommend Book
-            </h2>
-            
-          </div>
-          
-          <div class="sub-section category-archive">
-            <h2 class="sub-header">
-              Category Archive
-            </h2>
-            
-          </div>
-          
-          <div class="sub-section yearly-archive">
-            <h2 class="sub-header">
-              Yearly Archive
-            </h2>
-          </div>
-          
-          <div class="sub-section recent-entries">
-            <h2 class="sub-header">
-              Recent Entries
-            </h2>
-          </div>
-        </div>
-      </div>
+<?php
+  include 'sub.php';
+?>
       
       <div class="global-footer">
         <p class="notes-amazon-associate">
