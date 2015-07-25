8b3706d274cb02221b449f92fb47f57930e2cb8a
yoichi kobayashi <info@tplh.net>
2015-07-25 16:18:26 +0900

    [add] footerもモジュール化。

--- dst/wp/wp-content/themes/jstarted.com/archive.php
+++ dst/wp/wp-content/themes/jstarted.com/archive.php
@@ -45,22 +45,9 @@
 ?>
       </div>
       
-      <div class="global-footer">
-        <p class="notes-amazon-associate">
-          jstarted.comはamazon.co.jpを宣伝しリンクすることによってサイトが紹介料を<br>
-          獲得できる手段を提供することを目的に設定されたアフィリエイト宣伝プログラムである、<br>
-          Amazonアソシエイト・プログラムの参加者です。
-        </p>
-        <p class"license">
-          <small>
-            <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.ja">
-              <img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" />
-            </a>
-            <br>
-            <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">jstarted.com</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.jstarted.com/" property="cc:attributionName" rel="cc:attributionURL">yoichi kobayashi</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.ja">Creative Commons 表示 3.0 非移植 License</a>.
-          </small>
-        </p>
-      </div>
+<?php
+  include 'footer.php';
+?>
     </div>
   </body>
 </html>
