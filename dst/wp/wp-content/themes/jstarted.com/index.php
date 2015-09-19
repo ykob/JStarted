8269f4718bfd58c107908e89e0068f1099a03404
yoichi kobayashi <info@tplh.net>
2015-09-19 22:34:13 +0900

    [edit] css設計用のダミーhtml追加。

--- dst/wp/wp-content/themes/jstarted.com/index.php
+++ dst/wp/wp-content/themes/jstarted.com/index.php
@@ -46,15 +46,18 @@
           <div class="article-list">
             <div class="article-list-item">
               <a href="">
-                <h2 class="article-item-header">
-                  
-                </h2>
-                
+                <p class="article-list-item__date">2015.1.24</p>
+                <h1 class="article-list-item__header">
+                  非プログラマが、今度こそクラスとインスタンスを理解する
+                </h1>
+                <div class="article-list-item__excerpt">
+                  <p>プログラマな人たちのよく口にする「クラス」や「インスタンス」というものをきちんと理解できないままでいましたが、つい最近になってやっとその理解が進んできました。そのヒントになった事柄についてまとめてみます。</p>
+                </div>
               </a>
             </div>
           </div>
         </div>
-        
+
 <?php
   include 'sub.php';
 ?>
